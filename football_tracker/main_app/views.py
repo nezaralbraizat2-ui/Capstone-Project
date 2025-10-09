7516fae7a0017d8798632a2ab61d6c1aba42e941
nezar albraizat <nezaralbraizat2@gmail.com>
2025-10-09 15:39:12 +0300

    add index html and style in indexhtml.css

--- football_tracker/main_app/views.py
+++ football_tracker/main_app/views.py
@@ -11,18 +11,21 @@ def about(request):
     return render(request, 'about.html')
 
 class Team:
-    def __init__(self, name, country, coach_name):
+    def __init__(self, name, country, coach_name, stadium=None):
         self.name = name
         self.country = country
         self.coach_name = coach_name
-   
-    teams = [
-       Team("barcelona", "Spain", "hansi Flick"),
-       Team("man-united", "England", "ruben amorim"),
-       Team("man-city", "England", "Pep Guardiola"),
-       Team("real-madrid", "Spain", "xabi alonso"),
-   ]
-    
-def team_index(request):
+        self.stadium = stadium
 
+
+# Create the teams list after the Team class is defined so we can instantiate Team
+Team.teams = [
+    Team("barcelona", "Spain", "hansi Flick", "Camp Nou"),
+    Team("man-united", "England", "ruben amorim", "Old Trafford"),
+    Team("man-city", "England", "Pep Guardiola", "Etihad Stadium"),
+    Team("real-madrid", "Spain", "xabi alonso", "Santiago Bernabéu"),
+]
+
+
+def team_index(request):
     return render(request, 'teams/index.html', { 'teams': Team.teams })
